d036e61998af077233cdb7d9485627349156b493
张锐 <stamen0630@gmail.com>
2015-02-01 09:31:31 +0800

    修正页面上的错误

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,9 @@
 $(function() {
-    $("#act").load("meeting/2014_11_22.html");
+    var act = {
+        "title": "昆明开发者 - 2014年度技术分享会",
+        "path": "meeting/2014_11_22.html"
+    }
+
+    $("title").html(act.title)
+    $("#act").load(act.path);
 })
